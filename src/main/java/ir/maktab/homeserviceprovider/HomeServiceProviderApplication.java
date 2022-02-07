c91580e323a5663852969fd22fd789759a3ff2b8
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-07 01:05:17 -0800

    ci: add init method for some test in future

--- src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
+++ src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
@@ -1,13 +1,23 @@
 package ir.maktab.homeserviceprovider;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomeServiceProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HomeServiceProviderApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init() {
+        return args ->
+        {
+
+        };
     }
 
 }
