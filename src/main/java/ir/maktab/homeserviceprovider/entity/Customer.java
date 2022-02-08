645c5e4e106a8381ee1a42623a102d8a068aaecf
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-08 11:03:42 -0800

    feat(entity): customer can have much order and review

--- src/main/java/ir/maktab/homeserviceprovider/entity/Customer.java
+++ src/main/java/ir/maktab/homeserviceprovider/entity/Customer.java
@@ -4,10 +4,20 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
 public class Customer extends User {
+
+    @OneToMany(mappedBy = "customer")
+    private Set<Order> orders = new HashSet<>();
+
+    @OneToMany(mappedBy = "customer")
+    private Set<Review> reviews = new HashSet<>();
+
     @Builder(builderMethodName = "customerBuilder")
     public Customer(String firstname, String lastname, String email, String password, UserStatus status, String customerFiled) {
         super(firstname, lastname, email, password, status);
