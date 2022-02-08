424d6091da0fbceeb652baefc7a3d5c2598fe6c7
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-08 11:07:28 -0800

    feat(entity): the expert can offer for order and receives review

--- src/main/java/ir/maktab/homeserviceprovider/entity/Expert.java
+++ src/main/java/ir/maktab/homeserviceprovider/entity/Expert.java
@@ -4,10 +4,20 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
 public class Expert extends User {
+
+    @OneToMany(mappedBy = "expert")
+    private Set<Offer> offers = new HashSet<>();
+
+    @OneToMany(mappedBy = "expert")
+    private Set<Review> reviews = new HashSet<>();
+
     @Builder(builderMethodName = "expertBuilder")
     public Expert(String firstname, String lastname, String email, String password, UserStatus status) {
         super(firstname, lastname, email, password, status);
