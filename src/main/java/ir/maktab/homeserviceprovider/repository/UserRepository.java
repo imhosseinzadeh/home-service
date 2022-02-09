36bbb4c6e832b6e1240729a2efbbfb57628a24bb
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-09 08:47:12 -0800

    feat(repository): add password change feature for users

--- src/main/java/ir/maktab/homeserviceprovider/repository/UserRepository.java
+++ src/main/java/ir/maktab/homeserviceprovider/repository/UserRepository.java
@@ -2,8 +2,14 @@ package ir.maktab.homeserviceprovider.repository;
 
 import ir.maktab.homeserviceprovider.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE User u SET u.password= :password WHERE u.id= :id")
+    void updatePasswordById(Long id, String password);
 }
