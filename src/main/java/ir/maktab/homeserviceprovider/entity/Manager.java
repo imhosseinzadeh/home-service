788854935c6e79af346d48d704682f69e581fd55
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-09 07:37:12 -0800

    refactor(entity): rename wrong name

--- src/main/java/ir/maktab/homeserviceprovider/entity/Manager.java
+++ src/main/java/ir/maktab/homeserviceprovider/entity/Manager.java
@@ -8,9 +8,9 @@ import java.io.Serializable;
 
 @Entity
 @NoArgsConstructor
-public class Manger extends User implements Serializable {
+public class Manager extends User implements Serializable {
     @Builder(builderMethodName = "managerBuilder")
-    public Manger(String firstname, String lastname, String email, String password, UserStatus status) {
+    public Manager(String firstname, String lastname, String email, String password, UserStatus status) {
         super(firstname, lastname, email, password, status);
     }
 }
