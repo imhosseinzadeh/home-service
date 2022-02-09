93acfcf72a4aac432f8aeb27516cdcc1aad6ee2c
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-08 23:33:05 -0800

    feat(entity): define order status for order

--- src/main/java/ir/maktab/homeserviceprovider/entity/OrderStatus.java
+++ src/main/java/ir/maktab/homeserviceprovider/entity/OrderStatus.java
@@ -1,4 +1,20 @@
 package ir.maktab.homeserviceprovider.entity;
 
 public enum OrderStatus {
+    WAITING_FOR_EXPERTS_SUGGESTIONS(1),
+    WAITING_FOR_CHOICE_EXPERT(2),
+    WAITING_FOR_EXPERT_TO_COME_TO_YOUR_PLACE(3),
+    STARTED(3),
+    DONE(4),
+    PAID(5);
+
+    private int value;
+
+    private OrderStatus(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
