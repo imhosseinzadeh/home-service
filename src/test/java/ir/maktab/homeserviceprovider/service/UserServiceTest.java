9077cce59e97dd5fb1b926e97e000ea09d6969b5
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-09 15:02:57 -0800

    test(service): add test for find user by firstname and lastname feature

--- src/test/java/ir/maktab/homeserviceprovider/service/UserServiceTest.java
+++ src/test/java/ir/maktab/homeserviceprovider/service/UserServiceTest.java
@@ -6,6 +6,9 @@ import ir.maktab.homeserviceprovider.entity.UserStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
@@ -20,14 +23,22 @@ class UserServiceTest {
     private UserService<User> service;
 
     @Test
-    void updatePasswordById_PasswordChange_UpdatePassword() {
-        Long userId = saveUser("alireza@gmail.com", "abcd1234", UserStatus.NEW).getId();
+    void updatePasswordById_BeChange_IfPasswordUpdated() {
+        User user = User.builder()
+                .firstname("firstname")
+                .lastname("lastname")
+                .email("example@gmail.com")
+                .password("password1234")
+                .status(UserStatus.NEW)
+                .build();
+        Long userId = service.saveOrUpdate(user).getId();
 
         String newPass = "newPassword1234";
         service.updatePasswordById(userId, newPass);
 
         if (service.load(userId).isPresent()) {
             User loadUser = service.load(userId).get();
+
             assertEquals(loadUser.getPassword(), newPass);
         } else {
             fail("User not found");
@@ -35,23 +46,38 @@ class UserServiceTest {
     }
 
     @Test
-    void findByEmail_SavedUserAndLoadUserEqual_SaveAndLoadUser() {
-        String userEmail = "alireza@gmail.com";
-        User saveUser = saveUser(userEmail, "abcd1234", UserStatus.NEW);
+    void findByEmail_BeFound_IfAUserSaved() {
+        String userEmail = "example@gmail.com";
+        User user = User.builder()
+                .firstname("firstname")
+                .lastname("lastname")
+                .email(userEmail)
+                .password("password1234")
+                .status(UserStatus.NEW)
+                .build();
+        User savedUser = service.saveOrUpdate(user);
 
         User loadUser = service.findByEmail(userEmail);
 
-        assertEquals(saveUser, loadUser);
+        assertEquals(savedUser, loadUser);
     }
 
-    private User saveUser(String email, String password, UserStatus status) {
+    @Test
+    void findAllByFirstnameAndLastname_BeFound_IfAUserSaved() {
+        String firstname = "firstname";
+        String lastname = "lastname";
         User user = User.builder()
-                .firstname("iman")
-                .lastname("hosseinzadeh")
-                .email(email)
-                .password(password)
-                .status(status)
+                .firstname(firstname)
+                .lastname(lastname)
+                .email("example@gmail.com")
+                .password("password1234")
+                .status(UserStatus.NEW)
                 .build();
-        return service.saveOrUpdate(user);
+        User savedUser = service.saveOrUpdate(user);
+
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<User> userPage = service.findAllByFirstnameAndLastname(firstname, lastname, pageable);
+
+        assertTrue(userPage.get().anyMatch(loadUser -> loadUser.equals(savedUser)));
     }
 }
