8aa363c873988cf3c233dea2dbd2be3ba5ee3332
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-09 00:52:22 -0800

    feat(entity): the expert can have an image

--- src/main/java/ir/maktab/homeserviceprovider/entity/Expert.java
+++ src/main/java/ir/maktab/homeserviceprovider/entity/Expert.java
@@ -1,14 +1,15 @@
 package ir.maktab.homeserviceprovider.entity;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Getter
 @NoArgsConstructor
 public class Expert extends User {
 
@@ -18,8 +19,13 @@ public class Expert extends User {
     @OneToMany(mappedBy = "expert")
     private Set<Review> reviews = new HashSet<>();
 
+    @Lob
+    @Column(columnDefinition = "mediumblob")
+    private byte[] image;
+
     @Builder(builderMethodName = "expertBuilder")
-    public Expert(String firstname, String lastname, String email, String password, UserStatus status) {
+    public Expert(String firstname, String lastname, String email, String password, UserStatus status, byte[] image) {
         super(firstname, lastname, email, password, status);
+        this.image = image;
     }
 }
