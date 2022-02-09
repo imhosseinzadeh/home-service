cc645531dd68bcab7081b376682602e952789d76
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-09 08:50:26 -0800

    test(service): add test for check password change feature

---
+++ src/test/java/ir/maktab/homeserviceprovider/service/UserServiceTest.java
@@ -0,0 +1,40 @@
+package ir.maktab.homeserviceprovider.service;
+
+import ir.maktab.homeserviceprovider.config.AppUnitTestConfig;
+import ir.maktab.homeserviceprovider.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest//find beans in repository layer
+@ActiveProfiles(value = "test")
+@SpringJUnitConfig(value = AppUnitTestConfig.class)//find beans in service layer
+class UserServiceTest {
+
+    @Autowired
+    private UserService service;
+
+    @Test
+    void updatePasswordById_PasswordChange_UpdatePassword() {
+        User user = User.builder()
+                .firstname("iman")
+                .lastname("hosseinzadeh")
+                .password("12345678")
+                .build();
+        Long userId = service.saveOrUpdate(user).getId();
+
+        String newPass = "newPassword";
+        service.updatePasswordById(userId, newPass);
+
+        if (service.load(userId).isPresent()) {
+            User loadUser = service.load(userId).get();
+            assertEquals(loadUser.getPassword(), newPass);
+        } else {
+            fail("User not found");
+        }
+    }
+}
