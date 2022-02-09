638da7164a8445bcaf167026ca0b33aaeccda66e
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-09 10:42:50 -0800

    test(): add manual test for check expert image

--- src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
+++ src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
@@ -1,13 +1,19 @@
 package ir.maktab.homeserviceprovider;
 
 import ir.maktab.homeserviceprovider.entity.Customer;
+import ir.maktab.homeserviceprovider.entity.Expert;
 import ir.maktab.homeserviceprovider.service.CustomerService;
+import ir.maktab.homeserviceprovider.service.ExpertService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -15,6 +21,9 @@ public class HomeServiceProviderApplication {
 
     @Autowired
     CustomerService customerService;
+
+    @Autowired
+    ExpertService expertService;
 
     public static void main(String[] args) {
         SpringApplication.run(HomeServiceProviderApplication.class, args);
@@ -26,19 +35,50 @@ public class HomeServiceProviderApplication {
         {
             Customer customer = Customer.customerBuilder()
                     .firstname("iman")
-                    .lastname("hosseizadeh").
+                    .lastname("hosseinzadeh").
                     password("1234").
                     build();
             customerService.saveOrUpdate(customer);
 
-            Thread.sleep(20000);
+            Thread.sleep(10000);
 
             Optional<Customer> customer2 = customerService.load(1L);
             if (customer2.isPresent()) {
-                customer2.get().setEmail("eee");
+                customer2.get().setEmail("eeej");
                 customerService.saveOrUpdate(customer2.get());
             }
         };
-    }
+/*
+        return args -> {
+            System.out.println("Hibernate save image into database");
 
+            File file = new File("D:\\photo_2022-02-07_14-29-22.jpg");
+            byte[] bFile = new byte[(int) file.length()];
+
+            try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                //convert file into array of bytes
+                fileInputStream.read(bFile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Expert expert = Expert.expertBuilder()
+                    .image(bFile)
+                    .password("1234")
+                    .build();
+
+            Expert savedExpert = expertService.saveOrUpdate(expert);
+
+            Optional<Expert> expert2 = expertService.load(savedExpert.getId());
+            byte[] bExpert = expert2.get().getImage();
+
+            try (FileOutputStream fos = new FileOutputStream("D:\\test5.jpg")) {
+                fos.write(bExpert);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        };
+*/
+    }
 }
