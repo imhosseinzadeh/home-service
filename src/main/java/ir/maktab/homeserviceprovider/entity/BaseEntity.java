347fa148705f180af476098e6ea8c53f72a0dc34
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-10 19:39:52 +0330

    feat(entity): implement Serializable to make all entities Serializable

--- src/main/java/ir/maktab/homeserviceprovider/entity/BaseEntity.java
+++ src/main/java/ir/maktab/homeserviceprovider/entity/BaseEntity.java
@@ -2,6 +2,6 @@ package ir.maktab.homeserviceprovider.entity;
 
 import java.io.Serializable;
 
-public interface BaseEntity<I extends Serializable> {
+public interface BaseEntity<I extends Serializable> extends Serializable {
     I getId();
 }
