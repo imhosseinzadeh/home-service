06ae9d97174d7d34a96cf5003c6130142abdcc20
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-11 02:51:54 +0330

    feat(model): add getter and setter

--- src/main/java/ir/maktab/homeserviceprovider/model/ExpertServiceModelId.java
+++ src/main/java/ir/maktab/homeserviceprovider/model/ExpertServiceModelId.java
@@ -1,15 +1,19 @@
 package ir.maktab.homeserviceprovider.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 import java.io.Serializable;
 
 @Embeddable
-@RequiredArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class ExpertServiceModelId implements Serializable {
     private Long expertId;
     private Long serviceId;
