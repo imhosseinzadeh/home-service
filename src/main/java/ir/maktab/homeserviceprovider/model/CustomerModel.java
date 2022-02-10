94b71c031a821fb8728867bd25d11a40d41c41cc
imanfirstgit <89960731+imanfirstgit@users.noreply.github.com>
2022-02-11 01:40:41 +0330

    feat(model): add sync methods  to keep relational models in sync

--- src/main/java/ir/maktab/homeserviceprovider/model/CustomerModel.java
+++ src/main/java/ir/maktab/homeserviceprovider/model/CustomerModel.java
@@ -1,6 +1,7 @@
 package ir.maktab.homeserviceprovider.model;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
@@ -10,6 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity(name = "Customer")
+@Getter
 @NoArgsConstructor
 public class CustomerModel extends UserModel {
 
@@ -22,5 +24,31 @@ public class CustomerModel extends UserModel {
     @Builder(builderMethodName = "customerBuilder")
     public CustomerModel(String firstname, String lastname, String email, String password, UserModelStatus status, String customerFiled) {
         super(firstname, lastname, email, password, status);
+    }
+
+    public void setOrders(Set<OrderModel> orders) {
+        orders.forEach(this::addOrder);
+        this.orders = orders;
+    }
+
+    public void setReviews(Set<ReviewModel> reviews) {
+        reviews.forEach(this::addReview);
+        this.reviews = reviews;
+    }
+
+    public void addOrder(OrderModel order) {
+        order.setCustomer(this);
+    }
+
+    public void removeOrder(OrderModel order) {
+        this.orders.remove(order);
+    }
+
+    public void addReview(ReviewModel review) {
+        review.setCustomer(this);
+    }
+
+    public void removeReview(ReviewModel review) {
+        this.reviews.remove(review);
     }
 }
