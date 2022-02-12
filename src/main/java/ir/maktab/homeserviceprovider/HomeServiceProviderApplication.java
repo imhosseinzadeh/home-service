0ecef4151b288373bff16a75629c217b58bfeaad
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-12 10:17:43 +0330

    style: optimize imports

--- src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
+++ src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
@@ -4,16 +4,12 @@ import ir.maktab.homeserviceprovider.model.UserModel;
 import ir.maktab.homeserviceprovider.model.UserModelStatus;
 import ir.maktab.homeserviceprovider.model.WalletModel;
 import ir.maktab.homeserviceprovider.service.ExpertService;
-
 import ir.maktab.homeserviceprovider.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-
-
 
 import java.math.BigDecimal;
 import java.util.Optional;
