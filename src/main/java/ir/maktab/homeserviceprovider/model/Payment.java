260f543d7b95039a7e0fbe2e219df420bcfb81e3
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-13 11:07:16 +0330

    feat(model): used to pay Order price

---
+++ src/main/java/ir/maktab/homeserviceprovider/model/Payment.java
@@ -0,0 +1,40 @@
+package ir.maktab.homeserviceprovider.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment extends BaseModel<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private WalletModel srcWallet;
+
+    @ManyToOne
+    private WalletModel trgWallet;
+
+    private BigDecimal amount;
+
+    @Override
+    public void onPersist() {
+        super.onPersist();
+        transfer();
+    }
+
+    private void transfer() {
+        // TODO: 2/13/2022 complete transfer method
+        srcWallet.setBalance(srcWallet.getBalance().subtract(amount));
+        trgWallet.setBalance(trgWallet.getBalance().add(amount));
+    }
+
+}
