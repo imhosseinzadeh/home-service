607edad1eb1d34693ceefa7e104c6e5a0955e86c
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-16 19:13:18 +0330

    feat(service): add service stereotype

--- src/main/java/ir/maktab/homeserviceprovider/service/wallet/TransactionService.java
+++ src/main/java/ir/maktab/homeserviceprovider/service/wallet/TransactionService.java
@@ -3,7 +3,9 @@ package ir.maktab.homeserviceprovider.service.wallet;
 import ir.maktab.homeserviceprovider.model.wallet.TransactionModel;
 import ir.maktab.homeserviceprovider.repository.wallet.TransactionRepository;
 import ir.maktab.homeserviceprovider.service.BaseService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TransactionService extends BaseService<TransactionModel, Long> {
 
     private TransactionRepository repository;
