352ef47af2dfd1911f9286add590fbf6727f3b70
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-18 14:27:47 +0330

    feat: update CommandLineRunner

--- src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
+++ src/main/java/ir/maktab/homeserviceprovider/HomeServiceProviderApplication.java
@@ -1,24 +1,22 @@
 package ir.maktab.homeserviceprovider;
 
-import ir.maktab.homeserviceprovider.model.user.UserModel;
-import ir.maktab.homeserviceprovider.model.user.UserModelStatus;
-import ir.maktab.homeserviceprovider.model.wallet.WalletModel;
+import ir.maktab.homeserviceprovider.dto.user.CustomerDto;
+import ir.maktab.homeserviceprovider.model.user.CustomerModel;
+import ir.maktab.homeserviceprovider.service.user.CustomerService;
 import ir.maktab.homeserviceprovider.service.user.ExpertService;
-import ir.maktab.homeserviceprovider.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.math.BigDecimal;
 import java.util.Optional;
 
 @SpringBootApplication
 public class HomeServiceProviderApplication {
 
     @Autowired
-    UserService<UserModel> userService;
+    CustomerService customerService;
 
     @Autowired
     ExpertService expertService;
@@ -31,54 +29,16 @@ public class HomeServiceProviderApplication {
     public CommandLineRunner init() {
         return args ->
         {
-            WalletModel wallet = new WalletModel();
-            wallet.setBalance(BigDecimal.valueOf(10000));
-
-            UserModel user = UserModel.builder()
-                    .firstname("iman")
-                    .lastname("hosseinzadeh")
-                    .password("abcd1234")
-                    .email("ali@gmail.com")
-                    .status(UserModelStatus.NEW)
-                    .wallet(wallet)
+            CustomerDto dto = CustomerDto.builder()
+                    .email("ali111@gmail.com")
+                    .password("abc12345678")
                     .build();
 
-            Long id = userService.save(user).getId();
+            Long id = customerService.save(dto).getId();
 
-            Optional<UserModel> user2 = userService.load(id);
-            user2.ifPresent(System.out::println);
+            Optional<CustomerModel> customer2 = customerService.load(id);
+            customer2.ifPresent(System.out::println);
         };
-/*
-        return args -> {
-            System.out.println("Hibernate save image into database");
-
-            File file = new File("D:\\photo_2022-02-07_14-29-22.jpg");
-            byte[] bFile = new byte[(int) file.length()];
-
-            try (FileInputStream fileInputStream = new FileInputStream(file)) {
-                //convert file into array of bytes
-                fileInputStream.read(bFile);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            Expert expert = Expert.expertBuilder()
-                    .image(bFile)
-                    .password("1234")
-                    .build();
-
-            Expert savedExpert = expertService.saveOrUpdate(expert);
-
-            Optional<Expert> expert2 = expertService.load(savedExpert.getId());
-            byte[] bExpert = expert2.get().getImage();
-
-            try (FileOutputStream fos = new FileOutputStream("D:\\test5.jpg")) {
-                fos.write(bExpert);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        };
-*/
     }
 }
+
