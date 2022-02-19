1076a36ae1d051f4efb80429a39c4a1a548bbdf6
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-19 18:57:13 +0330

    feat(service): imp IUserController

--- src/main/java/ir/maktab/homeserviceprovider/controller/ExpertController.java
+++ src/main/java/ir/maktab/homeserviceprovider/controller/ExpertController.java
@@ -1,8 +1,8 @@
 package ir.maktab.homeserviceprovider.controller;
 
-import ir.maktab.homeserviceprovider.dto.user.CustomerDto;
+import ir.maktab.homeserviceprovider.dto.user.ExpertDto;
 import ir.maktab.homeserviceprovider.exception.DataNotExistsException;
-import ir.maktab.homeserviceprovider.service.user.CustomerService;
+import ir.maktab.homeserviceprovider.service.user.ExpertService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,30 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/experts")
 @RequiredArgsConstructor
-public class CustomerController implements IUserController<CustomerDto> {
+public class ExpertController implements IUserController<ExpertDto> {
 
-    private final CustomerService service;
+    private final ExpertService service;
 
     @Override
-    public ResponseEntity<CustomerDto> signUp(CustomerDto registerDto) {
-        Optional<CustomerDto> optSaved = service.save(registerDto);
+    public ResponseEntity<ExpertDto> signUp(ExpertDto registerDto) {
+        Optional<ExpertDto> optSaved = service.save(registerDto);
         return optSaved.map(dto -> ResponseEntity.status(HttpStatus.CREATED).body(dto))
                 .orElse(null);
     }
 
     @Override
-    public ResponseEntity<CustomerDto> getProfile(Long id) {
-        Optional<CustomerDto> optLoaded = this.service.load(id);
+    public ResponseEntity<ExpertDto> getProfile(Long id) {
+        Optional<ExpertDto> optLoaded = this.service.load(id);
         return optLoaded.map(ResponseEntity::ok)
                 .orElse(null);
     }
 
     @Override
-    public ResponseEntity<CustomerDto> updateProfile(Long id, CustomerDto customerDto) {
-        customerDto.setId(id);
-        Optional<CustomerDto> optUpdated = this.service.update(customerDto);
+    public ResponseEntity<ExpertDto> updateProfile(Long id, ExpertDto dto) {
+        dto.setId(id);
+        Optional<ExpertDto> optUpdated = this.service.update(dto);
         return optUpdated.map(ResponseEntity::ok)
                 .orElse(null);
     }
@@ -44,9 +44,9 @@ public class CustomerController implements IUserController<CustomerDto> {
     public ResponseEntity<String> changePassword(Long id, String newPassword) {
         try {
             service.updatePasswordById(id, newPassword);
-            return ResponseEntity.ok("Customer password changed successfully");
+            return ResponseEntity.ok("Expert password changed successfully");
         } catch (DataNotExistsException e) {
-            return ResponseEntity.ok("Customer with id:" + id + " does not exist");
+            return ResponseEntity.ok("Expert with id:" + id + " does not exist");
         }
     }
 
@@ -54,11 +54,9 @@ public class CustomerController implements IUserController<CustomerDto> {
     public ResponseEntity<String> deleteAccount(Long id) {
         try {
             this.service.deleteById(id);
-            return ResponseEntity.ok("Customer account has been successfully deleted");
+            return ResponseEntity.ok("Expert account has been successfully deleted");
         } catch (DataNotExistsException e) {
-            return ResponseEntity.ok("Customer with id:" + id + " does not exist");
+            return ResponseEntity.ok("Expert with id:" + id + " does not exist");
         }
     }
-
 }
-
