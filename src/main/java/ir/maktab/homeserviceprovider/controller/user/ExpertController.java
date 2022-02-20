8184572127abf7ff9c2953b9ae1529d56db854aa
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-20 11:06:29 +0330

    feat(controller): add validation exception handler for ExpetController

--- src/main/java/ir/maktab/homeserviceprovider/controller/user/ExpertController.java
+++ src/main/java/ir/maktab/homeserviceprovider/controller/user/ExpertController.java
@@ -8,9 +8,16 @@ import ir.maktab.homeserviceprovider.exception.WrongDataInputException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -61,4 +68,18 @@ public class ExpertController implements IUserController<ExpertDto> {
             return ResponseEntity.ok("Expert with id:" + id + " does not exist");
         }
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
 }
