2683c47c4572e349eb0d6d78924c189db349d71b
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-20 20:42:45 +0330

    feat(dto): add constructor who call the super constructor

--- src/main/java/ir/maktab/homeserviceprovider/dto/user/CustomerDto.java
+++ src/main/java/ir/maktab/homeserviceprovider/dto/user/CustomerDto.java
@@ -1,13 +1,25 @@
 package ir.maktab.homeserviceprovider.dto.user;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-@AllArgsConstructor
 public class CustomerDto extends UserDto {
+    public CustomerDto(@Email(message = "UserDto-email not valid")
+                       @NotBlank(message = "UserDto-email cannot be blank")
+                               String email,
+                       @NotBlank(message = "UserDto-password cannot be null")
+                       @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "Incorrect pattern for UserDto-password")
+                               String password) {
+        super(email, password);
+    }
 }
--- src/main/java/ir/maktab/homeserviceprovider/dto/user/ExpertDto.java
+++ src/main/java/ir/maktab/homeserviceprovider/dto/user/ExpertDto.java
@@ -1,17 +1,18 @@
 package ir.maktab.homeserviceprovider.dto.user;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.PositiveOrZero;
 
 @Data
 @SuperBuilder
-@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class ExpertDto extends UserDto {
@@ -20,4 +21,17 @@ public class ExpertDto extends UserDto {
     @PositiveOrZero(message = "ExpertDto-score must be positive or zero")
     @Range(min = 0, max = 5)
     private Integer score;
+
+    public ExpertDto(@Email(message = "UserDto-email not valid")
+                     @NotBlank(message = "UserDto-email cannot be blank")
+                             String email,
+                     @NotBlank(message = "UserDto-password cannot be null")
+                     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "Incorrect pattern for UserDto-password")
+                             String password,
+                     byte[] image,
+                     Integer score) {
+        super(email, password);
+        this.image = image;
+        this.score = score;
+    }
 }
