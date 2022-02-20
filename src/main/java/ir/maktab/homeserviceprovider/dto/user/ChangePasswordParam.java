77f8cd76f62c5bc22b6a580a1931bd3638662140
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-20 10:17:39 +0330

    feat(dto): add dto for change user password

---
+++ src/main/java/ir/maktab/homeserviceprovider/dto/user/ChangePasswordParam.java
@@ -0,0 +1,22 @@
+package ir.maktab.homeserviceprovider.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePasswordParam {
+    @NotBlank(message = "ChangePasswordParam-oldPassword cannot be blank")
+    private String oldPassword;
+    @NotBlank(message = "ChangePasswordParam-newPassword cannot be blank")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "Incorrect pattern for ChangePasswordParam-newPassword")
+    private String newPassword;
+
+}
