93edc9eb5cf90e5c4e61bec2895ea474876810a2
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-21 19:37:30 +0330

    build(config): use UtilityClass annotations

--- src/main/java/ir/maktab/homeserviceprovider/config/ApplicationInfo.java
+++ src/main/java/ir/maktab/homeserviceprovider/config/ApplicationInfo.java
@@ -1,7 +1,9 @@
 package ir.maktab.homeserviceprovider.config;
 
+import lombok.experimental.UtilityClass;
 import springfox.documentation.service.Contact;
 
+@UtilityClass
 public class ApplicationInfo {
     static final Contact CONTACT = new Contact("Iman Hosseinzadeh", "ImanHosseinzadeh.com", "Imhosseinzadeh@gmail.com");
     static final String DESCRIPTION = "HomeServiceProvider is a home service system";
@@ -9,7 +11,4 @@ public class ApplicationInfo {
     static final String LICENSE_URL = "License Url";
     static final String VERSION = "1.0";
     static final String TITLE = "HomeServiceProvider";
-
-    private ApplicationInfo() {
-    }
 }
