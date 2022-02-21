d81a20c21f2978b91afd1646ee8a876bcc8be7fc
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-21 19:27:14 +0330

    build(config): add for SpringFoxConfig

---
+++ src/main/java/ir/maktab/homeserviceprovider/config/ApplicationInfo.java
@@ -0,0 +1,15 @@
+package ir.maktab.homeserviceprovider.config;
+
+import springfox.documentation.service.Contact;
+
+public class ApplicationInfo {
+    static final Contact CONTACT = new Contact("Iman Hosseinzadeh", "ImanHosseinzadeh.com", "Imhosseinzadeh@gmail.com");
+    static final String DESCRIPTION = "HomeServiceProvider is a home service system";
+    static final String LICENSE = "License 2022";
+    static final String LICENSE_URL = "License Url";
+    static final String VERSION = "1.0";
+    static final String TITLE = "HomeServiceProvider";
+
+    private ApplicationInfo() {
+    }
+}
