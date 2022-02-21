631fc6e52c5b416bbd00bcf9a0258f5f4c61bd38
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-21 20:24:32 +0330

    build(config): move to util package and make constants public

---
+++ src/main/java/ir/maktab/homeserviceprovider/util/ApplicationInfoUtil.java
@@ -0,0 +1,14 @@
+package ir.maktab.homeserviceprovider.util;
+
+import lombok.experimental.UtilityClass;
+import springfox.documentation.service.Contact;
+
+@UtilityClass
+public class ApplicationInfoUtil {
+    public static final Contact CONTACT = new Contact("Iman Hosseinzadeh", "ImanHosseinzadeh.com", "Imhosseinzadeh@gmail.com");
+    public static final String DESCRIPTION = "HomeServiceProvider is a home service system";
+    public static final String LICENSE = "License 2022";
+    public static final String LICENSE_URL = "License Url";
+    public static final String VERSION = "1.0";
+    public static final String TITLE = "HomeServiceProvider";
+}
