44951fce6d238384d02797f41c813ca41cd6f7d9
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-23 02:24:47 +0330

    feat(service): getRelatedOrders by some methods

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/user/ExpertService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/user/ExpertService.java
@@ -1,7 +1,9 @@
 package ir.maktab.homeserviceprovider.domain.service.user;
 
+import ir.maktab.homeserviceprovider.domain.model.order.OrderModel;
 import ir.maktab.homeserviceprovider.domain.model.service.ExpertServiceModel;
 import ir.maktab.homeserviceprovider.domain.model.service.ServiceModel;
+import ir.maktab.homeserviceprovider.domain.model.service.SubServiceModel;
 import ir.maktab.homeserviceprovider.domain.model.user.ExpertModel;
 import ir.maktab.homeserviceprovider.dto.user.ExpertDto;
 import ir.maktab.homeserviceprovider.repository.service.ExpertServiceRepository;
@@ -10,6 +12,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ExpertService extends UserService<ExpertModel, ExpertDto> {
@@ -34,9 +39,41 @@ public class ExpertService extends UserService<ExpertModel, ExpertDto> {
     }
 
     @Transactional(readOnly = true)
+    public List<OrderModel> getRelatedOrders(ExpertModel expert, Pageable pageable) {
+        Page<ServiceModel> relatedServices = findServicesByExpert(expert, pageable);
+        List<SubServiceModel> relatedSubServices = getSubServices(relatedServices);
+        return getOrders(relatedSubServices);
+    }
+
+    private List<OrderModel> getOrders(List<SubServiceModel> subServiceModelList) {
+        List<OrderModel> orders = new ArrayList<>();
+
+        for (SubServiceModel s : subServiceModelList) {
+            orders.addAll(s.getOrders());
+        }
+
+        return orders;
+    }
+
+    private List<SubServiceModel> getSubServices(Page<ServiceModel> services) {
+        List<SubServiceModel> subServiceList = new ArrayList<>();
+
+        for (ServiceModel s : services) {
+            subServiceList.addAll(s.getSubServices());
+        }
+
+        return subServiceList;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ServiceModel> findServicesByExpert(ExpertModel expert, Pageable pageable) {
+        Page<ExpertServiceModel> allByExpert = expertServiceRepository.findAllByExpert(expert, pageable);
+        return allByExpert.map(ExpertServiceModel::getService);
+    }
+
+    @Transactional(readOnly = true)
     public Page<ExpertModel> findExpertsByService(ServiceModel service, Pageable pageable) {
         Page<ExpertServiceModel> allByService = expertServiceRepository.findAllByService(service, pageable);
         return allByService.map(ExpertServiceModel::getExpert);
     }
-
 }
