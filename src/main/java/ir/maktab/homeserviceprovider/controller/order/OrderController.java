ffcb5f2acc66562f78b005550cd4175125c32fab
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-23 00:46:44 +0330

    feat(controller): imp crud

--- src/main/java/ir/maktab/homeserviceprovider/controller/order/OrderController.java
+++ src/main/java/ir/maktab/homeserviceprovider/controller/order/OrderController.java
@@ -1,9 +1,15 @@
 package ir.maktab.homeserviceprovider.controller.order;
 
 import ir.maktab.homeserviceprovider.domain.service.order.OrderService;
+import ir.maktab.homeserviceprovider.dto.order.OrderDto;
+import ir.maktab.homeserviceprovider.exception.DataNotExistsException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/orders")
@@ -12,4 +18,49 @@ public class OrderController {
 
     private final OrderService service;
 
+    @PostMapping("/submit")
+    ResponseEntity<OrderDto> submit(@RequestBody @Valid OrderDto dto) {
+        Optional<OrderDto> optSaved = service.save(dto);
+        return optSaved
+                .map(savedDto -> ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body(savedDto))
+                .orElse(ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(null));
+    }
+
+    //detail
+    @GetMapping("/detail/{id}")
+    ResponseEntity<OrderDto> getOrder(@PathVariable Long id) {
+        Optional<OrderDto> optLoaded = this.service.load(id);
+        return optLoaded
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(null));
+    }
+
+    @PutMapping("/detail/{id}")
+    ResponseEntity<OrderDto> updateOrder(@PathVariable Long id, @RequestBody @Valid OrderDto dto) {
+        dto.setId(id);
+        Optional<OrderDto> optUpdated = this.service.update(dto);
+        return optUpdated
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(null));
+    }
+
+    @DeleteMapping("/delete-order/{id}")
+    ResponseEntity<String> deleteOrder(@PathVariable Long id) {
+        try {
+            this.service.deleteById(id);
+            return ResponseEntity.ok("Order has been successfully deleted");
+        } catch (DataNotExistsException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Order with id: " + id + " does not exist");
+        }
+    }
 }
