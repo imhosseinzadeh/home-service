15e2233737010688f1de673b5471503b31f205c9
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-22 16:14:06 +0330

    feat(model): override equals and hashcode

--- src/main/java/ir/maktab/homeserviceprovider/domain/model/service/ExpertServiceModelId.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/model/service/ExpertServiceModelId.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 @AllArgsConstructor
@@ -16,4 +17,18 @@ import java.io.Serializable;
 public class ExpertServiceModelId implements Serializable {
     private Long expertId;
     private Long serviceId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExpertServiceModelId that = (ExpertServiceModelId) o;
+        return Objects.equals(expertId, that.expertId) && Objects.equals(serviceId, that.serviceId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(expertId, serviceId);
+    }
+    
 }
