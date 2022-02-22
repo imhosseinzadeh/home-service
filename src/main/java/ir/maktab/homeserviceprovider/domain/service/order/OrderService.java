75e023a698cb9da9385840d4cb201aaba3d5188d
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-23 00:43:36 +0330

    feat(service): override save method to set orderStatus

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/order/OrderService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/order/OrderService.java
@@ -1,10 +1,13 @@
 package ir.maktab.homeserviceprovider.domain.service.order;
 
 import ir.maktab.homeserviceprovider.domain.model.order.OrderModel;
+import ir.maktab.homeserviceprovider.domain.model.order.OrderModelStatus;
 import ir.maktab.homeserviceprovider.domain.service.BaseService;
 import ir.maktab.homeserviceprovider.dto.order.OrderDto;
 import ir.maktab.homeserviceprovider.repository.order.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class OrderService extends BaseService<OrderModel, OrderDto, Long> {
@@ -14,6 +17,13 @@ public class OrderService extends BaseService<OrderModel, OrderDto, Long> {
     public OrderService(OrderRepository orderRepository) {
         super(orderRepository);
         this.repository = orderRepository;
+    }
+
+    @Override
+    public Optional<OrderDto> save(OrderDto dto) {
+        dto.setStatus(OrderModelStatus.WAITING_FOR_EXPERTS_SUGGESTIONS);
+        OrderModel savedModel = repository.save(mapToModel(dto));
+        return Optional.ofNullable(mapToDto(savedModel));
     }
 
     @Override
