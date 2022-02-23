8238755aedb014c305413b43e7725c094a192a52
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-23 22:59:29 +0330

    feat(mapper): add mapper for ExpertModel and ExpertDto

---
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/ExpertMapper.java
@@ -0,0 +1,19 @@
+package ir.maktab.homeserviceprovider.mapper;
+
+import ir.maktab.homeserviceprovider.domain.model.user.ExpertModel;
+import ir.maktab.homeserviceprovider.dto.user.ExpertDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface ExpertMapper {
+
+    ExpertDto mapToDto(ExpertModel model);
+
+    ExpertModel mapToModel(ExpertDto dto);
+
+    void updateDtoByModel(@MappingTarget ExpertDto dto, ExpertModel model);
+
+    void updateModelByDto(@MappingTarget ExpertModel model, ExpertDto dto);
+
+}
