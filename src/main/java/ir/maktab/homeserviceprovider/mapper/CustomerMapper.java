d0262e3c3270b6e1fdcc0ce7e3a2a90f3c723bc0
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-23 20:13:26 +0330

    fix(mapper): add @MappingTarget

--- src/main/java/ir/maktab/homeserviceprovider/mapper/CustomerMapper.java
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/CustomerMapper.java
@@ -1,4 +1,4 @@
-package ir.maktab.homeserviceprovider.mapper.user;
+package ir.maktab.homeserviceprovider.mapper;
 
 import ir.maktab.homeserviceprovider.domain.model.user.CustomerModel;
 import ir.maktab.homeserviceprovider.dto.user.CustomerDto;
@@ -11,7 +11,7 @@ public interface CustomerMapper {
 
     CustomerModel mapToModel(CustomerDto dto);
 
-    void updateDto(CustomerModel model, CustomerDto dto);
+    void updateDtoByModel(CustomerModel model, @MappingTarget CustomerDto dto);
 
     void updateModelByDto(CustomerDto dto, @MappingTarget CustomerModel model);
 }
