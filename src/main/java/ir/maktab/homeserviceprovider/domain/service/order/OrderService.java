99478ea337ee300374bb2814d1b95714d108d6ba
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-23 23:57:25 +0330

    feat(service): imp BaseService abstract methods

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/order/OrderService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/order/OrderService.java
@@ -1,9 +1,9 @@
 package ir.maktab.homeserviceprovider.domain.service.order;
 
 import ir.maktab.homeserviceprovider.domain.model.order.OrderModel;
-import ir.maktab.homeserviceprovider.domain.model.order.OrderModelStatus;
 import ir.maktab.homeserviceprovider.domain.service.BaseService;
 import ir.maktab.homeserviceprovider.dto.order.OrderDto;
+import ir.maktab.homeserviceprovider.mapper.order.OrderMapper;
 import ir.maktab.homeserviceprovider.repository.order.OrderRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,30 +13,31 @@ import java.util.Optional;
 public class OrderService extends BaseService<OrderModel, OrderDto, Long> {
 
     private final OrderRepository repository;
+    private final OrderMapper mapper;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, OrderMapper mapper) {
         super(orderRepository);
         this.repository = orderRepository;
+        this.mapper = mapper;
     }
 
     @Override
     protected OrderDto mapToDto(OrderModel model) {
-        return null;
+        return this.mapper.mapToDto(model);
     }
 
     @Override
     protected OrderModel mapToModel(OrderDto dto) {
-        return null;
+        return this.mapper.mapToModel(dto);
     }
 
     @Override
     protected void updateModelByDto(OrderDto dto, OrderModel model) {
-
+        this.mapper.updateModelByDto(model, dto);
     }
 
     @Override
     public Optional<OrderDto> save(OrderDto dto) {
-        dto.setStatus(OrderModelStatus.WAITING_FOR_EXPERTS_SUGGESTIONS);
         OrderModel savedModel = repository.save(mapToModel(dto));
         return Optional.ofNullable(mapToDto(savedModel));
     }
