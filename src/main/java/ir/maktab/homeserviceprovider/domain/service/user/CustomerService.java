fcbcfd67c31bf782ba194b819151578e6916c3fd
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-23 22:16:00 +0330

    feat(service): start use the CustomerMapper

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/user/CustomerService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/user/CustomerService.java
@@ -2,6 +2,7 @@ package ir.maktab.homeserviceprovider.domain.service.user;
 
 import ir.maktab.homeserviceprovider.domain.model.user.CustomerModel;
 import ir.maktab.homeserviceprovider.dto.user.CustomerDto;
+import ir.maktab.homeserviceprovider.mapper.CustomerMapper;
 import ir.maktab.homeserviceprovider.repository.user.CustomerRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,24 +10,26 @@ import org.springframework.stereotype.Service;
 public class CustomerService extends UserService<CustomerModel, CustomerDto> {
 
     private final CustomerRepository repository;
+    private final CustomerMapper mapper;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CustomerMapper mapper) {
         super(customerRepository);
         this.repository = customerRepository;
+        this.mapper = mapper;
     }
 
     @Override
     protected CustomerDto mapToDto(CustomerModel model) {
-        return null;
+        return this.mapper.mapToDto(model);
     }
 
     @Override
     protected CustomerModel mapToModel(CustomerDto dto) {
-        return null;
+        return this.mapper.mapToModel(dto);
     }
 
     @Override
     protected void updateModelByDto(CustomerDto dto, CustomerModel model) {
-
+        this.mapper.updateModelByDto(model, dto);
     }
 }
