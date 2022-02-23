47ef9b229328e48fefb8dc55b1a2cbfa6561d153
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-24 00:19:53 +0330

    feat(mapper): map customer to customerId and subService to subServiceId

--- src/main/java/ir/maktab/homeserviceprovider/mapper/order/OrderMapper.java
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/order/OrderMapper.java
@@ -3,15 +3,20 @@ package ir.maktab.homeserviceprovider.mapper.order;
 import ir.maktab.homeserviceprovider.domain.model.order.OrderModel;
 import ir.maktab.homeserviceprovider.dto.order.OrderDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
 
+    @Mapping(target = "customerId", expression = "java(model.getCustomer().getId())")
+    @Mapping(target = "subServiceId", expression = "java(model.getSubService().getId())")
     OrderDto mapToDto(OrderModel model);
 
     OrderModel mapToModel(OrderDto dto);
 
+    @Mapping(target = "customerId", expression = "java(model.getCustomer().getId())")
+    @Mapping(target = "subServiceId", expression = "java(model.getSubService().getId())")
     void updateDtoByModel(@MappingTarget OrderDto dto, OrderModel model);
 
     void updateModelByDto(@MappingTarget OrderModel model, OrderDto dto);
