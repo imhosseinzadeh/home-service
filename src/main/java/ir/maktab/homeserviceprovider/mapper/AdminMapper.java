8b128a5a670bca3bbd3442c50206dcb1e850ed77
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-23 22:59:09 +0330

    feat(mapper): add mapper for AdminModel and AdminDto

---
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/AdminMapper.java
@@ -0,0 +1,19 @@
+package ir.maktab.homeserviceprovider.mapper;
+
+import ir.maktab.homeserviceprovider.domain.model.user.AdminModel;
+import ir.maktab.homeserviceprovider.dto.user.AdminDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface AdminMapper {
+
+    AdminDto mapToDto(AdminModel model);
+
+    AdminModel mapToModel(AdminDto dto);
+
+    void updateDtoByModel(@MappingTarget AdminDto dto, AdminModel model);
+
+    void updateModelByDto(@MappingTarget AdminModel model, AdminDto dto);
+
+}
