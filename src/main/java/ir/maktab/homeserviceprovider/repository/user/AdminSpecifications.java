e4a2cda92e7477694b88f0005ad60ec9c85cd3a7
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-24 13:16:15 +0330

    feat(repository): a class with methods to filter admin table by specifications

---
+++ src/main/java/ir/maktab/homeserviceprovider/repository/user/AdminSpecifications.java
@@ -0,0 +1,8 @@
+package ir.maktab.homeserviceprovider.repository.user;
+
+import ir.maktab.homeserviceprovider.domain.model.user.AdminModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminSpecifications extends UserSpecifications<AdminModel> {
+}
