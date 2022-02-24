e79b7a16f52df7d7226e01153d531be447418f68
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-24 20:03:16 +0330

    feat(service): impl findAll

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/user/AdminService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/user/AdminService.java
@@ -2,6 +2,7 @@ package ir.maktab.homeserviceprovider.domain.service.user;
 
 import ir.maktab.homeserviceprovider.domain.model.user.AdminModel;
 import ir.maktab.homeserviceprovider.dto.user.AdminDto;
+import ir.maktab.homeserviceprovider.dto.user.param.UserSearchParam;
 import ir.maktab.homeserviceprovider.mapper.user.AdminMapper;
 import ir.maktab.homeserviceprovider.repository.user.AdminRepository;
 import ir.maktab.homeserviceprovider.repository.user.AdminSpecifications;
@@ -9,8 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
 
 @Service
 public class AdminService extends UserService<AdminModel, AdminDto> {
@@ -42,12 +41,10 @@ public class AdminService extends UserService<AdminModel, AdminDto> {
     }
 
     @Override
-    public Page<AdminModel> findAll(Pageable pageable) {
+    public Page<AdminModel> findAll(UserSearchParam searchParam, Pageable pageable) {
         return repository.findAll(Specification.where(this.specifications
-                .withFirstname("test")
-                .and(specifications.withLastname("test"))
-                .and(specifications.withWalletBalance(new BigDecimal(1L)))
-                .and(specifications.withWalletBalanceGt(new BigDecimal(0)))
-                .and(specifications.withWalletBalanceLe(new BigDecimal(10L)))), pageable);
+                .withFirstname(searchParam.getFirstname())
+                .and(specifications.withLastname(searchParam.getLastname()))), pageable);
     }
+
 }
--- src/main/java/ir/maktab/homeserviceprovider/domain/service/user/CustomerService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/user/CustomerService.java
@@ -2,6 +2,7 @@ package ir.maktab.homeserviceprovider.domain.service.user;
 
 import ir.maktab.homeserviceprovider.domain.model.user.CustomerModel;
 import ir.maktab.homeserviceprovider.dto.user.CustomerDto;
+import ir.maktab.homeserviceprovider.dto.user.param.UserSearchParam;
 import ir.maktab.homeserviceprovider.mapper.user.CustomerMapper;
 import ir.maktab.homeserviceprovider.repository.user.CustomerRepository;
 import ir.maktab.homeserviceprovider.repository.user.CustomerSpecifications;
@@ -9,8 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
 
 @Service
 public class CustomerService extends UserService<CustomerModel, CustomerDto> {
@@ -42,12 +41,10 @@ public class CustomerService extends UserService<CustomerModel, CustomerDto> {
     }
 
     @Override
-    public Page<CustomerModel> findAll(Pageable pageable) {
+    public Page<CustomerModel> findAll(UserSearchParam searchParam, Pageable pageable) {
         return repository.findAll(Specification.where(this.specifications
-                .withFirstname("test")
-                .and(specifications.withLastname("test"))
-                .and(specifications.withWalletBalance(new BigDecimal(1L)))
-                .and(specifications.withWalletBalanceGt(new BigDecimal(0)))
-                .and(specifications.withWalletBalanceLe(new BigDecimal(10L)))), pageable);
+                .withFirstname(searchParam.getFirstname())
+                .and(specifications.withLastname(searchParam.getLastname()))), pageable);
     }
+
 }
