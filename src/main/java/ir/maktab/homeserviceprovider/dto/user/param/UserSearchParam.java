2f831683119361e9fdadd0c8b1437f63d45f3cc2
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-24 19:58:59 +0330

    feat(dto): add getters and setters

--- src/main/java/ir/maktab/homeserviceprovider/dto/user/param/UserSearchParam.java
+++ src/main/java/ir/maktab/homeserviceprovider/dto/user/param/UserSearchParam.java
@@ -1,8 +1,13 @@
 package ir.maktab.homeserviceprovider.dto.user.param;
 
-public class UserSearchParam {
+import ir.maktab.homeserviceprovider.dto.service.ServiceDto;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class UserSearchParam {
     private String firstname;
     private String Lastname;
-
+    private ServiceDto service;
 }
