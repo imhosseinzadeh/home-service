1dc0e54f8ebcbdf2a1cb1c2c156a23b03ec538d8
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-24 13:16:37 +0330

    feat(repository): a class with methods to filter expert table by specifications

---
+++ src/main/java/ir/maktab/homeserviceprovider/repository/user/ExpertSpecifications.java
@@ -0,0 +1,8 @@
+package ir.maktab.homeserviceprovider.repository.user;
+
+import ir.maktab.homeserviceprovider.domain.model.user.ExpertModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExpertSpecifications extends UserSpecifications<ExpertModel> {
+}
