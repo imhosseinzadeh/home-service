746b50a5ca29e2bf395355fd74038f4673a0de4c
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-24 13:16:24 +0330

    feat(repository): a class with methods to filter customer table by specifications

---
+++ src/main/java/ir/maktab/homeserviceprovider/repository/user/CustomerSpecifications.java
@@ -0,0 +1,8 @@
+package ir.maktab.homeserviceprovider.repository.user;
+
+import ir.maktab.homeserviceprovider.domain.model.user.CustomerModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerSpecifications extends UserSpecifications<CustomerModel> {
+}
