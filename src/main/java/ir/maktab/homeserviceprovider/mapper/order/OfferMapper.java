b431e2962e028a66cc39c9e2687c65df6a0b7852
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-26 20:29:57 +0330

    feat(mapper): for mapToModel, model get new created ExpertModel and OrderModel with the same id that they have in dto

--- src/main/java/ir/maktab/homeserviceprovider/mapper/order/OfferMapper.java
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/order/OfferMapper.java
@@ -9,16 +9,19 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring")
 public interface OfferMapper {
 
-
     @Mapping(target = "expertId", source = "expert.id")
     @Mapping(target = "orderId", source = "order.id")
     OfferDto mapToDto(OfferModel model);
 
+    @Mapping(target = "expert.id", source = "expertId")
+    @Mapping(target = "order.id", source = "orderId")
     OfferModel mapToModel(OfferDto dto);
 
     @Mapping(target = "expertId", source = "expert.id")
     @Mapping(target = "orderId", source = "order.id")
     void updateDtoByModel(@MappingTarget OfferDto dto, OfferModel model);
 
+    @Mapping(target = "expert.id", source = "expertId")
+    @Mapping(target = "order.id", source = "orderId")
     void updateModelByDto(@MappingTarget OfferModel model, OfferDto dto);
 }
