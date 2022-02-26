c7802579a5d2aa9f707c3fb2c98b63c8d6fc471b
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-26 18:44:16 +0330

    feat(mapper): impl mapper for offer

---
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/order/OfferMapper.java
@@ -0,0 +1,24 @@
+package ir.maktab.homeserviceprovider.mapper.order;
+
+import ir.maktab.homeserviceprovider.domain.model.order.OfferModel;
+import ir.maktab.homeserviceprovider.dto.order.OfferDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface OfferMapper {
+
+
+    @Mapping(target = "expertId", source = "expert.id")
+    @Mapping(target = "orderId", source = "order.id")
+    OfferDto mapToDto(OfferModel model);
+
+    OfferModel mapToModel(OfferDto dto);
+
+    @Mapping(target = "expertId", source = "expert.id")
+    @Mapping(target = "orderId", source = "order.id")
+    void updateDtoByModel(@MappingTarget OfferDto dto, OfferModel model);
+
+    void updateModelByDto(@MappingTarget OfferModel model, OfferDto dto);
+}
