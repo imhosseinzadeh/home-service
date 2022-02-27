a1a886a122b351dc8e79123d01d3513662075af9
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-27 21:03:31 +0330

    feat(service): impl mapper method

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/service/ServiceService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/service/ServiceService.java
@@ -3,6 +3,7 @@ package ir.maktab.homeserviceprovider.domain.service.service;
 import ir.maktab.homeserviceprovider.domain.model.service.ServiceModel;
 import ir.maktab.homeserviceprovider.domain.service.BaseService;
 import ir.maktab.homeserviceprovider.dto.service.ServiceDto;
+import ir.maktab.homeserviceprovider.mapper.service.ServiceMapper;
 import ir.maktab.homeserviceprovider.repository.service.ServiceRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,24 +11,26 @@ import org.springframework.stereotype.Service;
 public class ServiceService extends BaseService<ServiceModel, ServiceDto, Long> {
 
     private final ServiceRepository repository;
+    private final ServiceMapper mapper;
 
-    public ServiceService(ServiceRepository serviceRepository) {
+    public ServiceService(ServiceRepository serviceRepository, ServiceMapper mapper) {
         super(serviceRepository);
         this.repository = serviceRepository;
+        this.mapper = mapper;
     }
 
     @Override
     protected ServiceDto mapToDto(ServiceModel model) {
-        return null;
+        return mapper.mapToDto(model);
     }
 
     @Override
     protected ServiceModel mapToModel(ServiceDto dto) {
-        return null;
+        return mapper.mapToModel(dto);
     }
 
     @Override
     protected void updateModelByDto(ServiceDto dto, ServiceModel model) {
-
+        mapper.updateModelByDto(model, dto);
     }
 }
--- src/main/java/ir/maktab/homeserviceprovider/domain/service/service/SubServiceService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/service/SubServiceService.java
@@ -3,6 +3,7 @@ package ir.maktab.homeserviceprovider.domain.service.service;
 import ir.maktab.homeserviceprovider.domain.model.service.SubServiceModel;
 import ir.maktab.homeserviceprovider.domain.service.BaseService;
 import ir.maktab.homeserviceprovider.dto.service.SubServiceDto;
+import ir.maktab.homeserviceprovider.mapper.service.SubServiceMapper;
 import ir.maktab.homeserviceprovider.repository.service.SubServiceRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,25 +11,27 @@ import org.springframework.stereotype.Service;
 public class SubServiceService extends BaseService<SubServiceModel, SubServiceDto, Long> {
 
     private final SubServiceRepository repository;
+    private final SubServiceMapper mapper;
 
-    public SubServiceService(SubServiceRepository subServiceRepository) {
+    public SubServiceService(SubServiceRepository subServiceRepository, SubServiceMapper mapper) {
         super(subServiceRepository);
         this.repository = subServiceRepository;
+        this.mapper = mapper;
     }
 
 
     @Override
     protected SubServiceDto mapToDto(SubServiceModel model) {
-        return null;
+        return mapper.mapToDto(model);
     }
 
     @Override
     protected SubServiceModel mapToModel(SubServiceDto dto) {
-        return null;
+        return mapper.mapToModel(dto);
     }
 
     @Override
     protected void updateModelByDto(SubServiceDto dto, SubServiceModel model) {
-
+        mapper.updateModelByDto(model, dto);
     }
 }
