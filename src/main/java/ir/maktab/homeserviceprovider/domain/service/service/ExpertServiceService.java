9daa5b874eaba6d54bd0c123fbb0afd0b65a2336
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-27 18:45:16 +0330

    feat(service): impl repository methods

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/service/ExpertServiceService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/service/ExpertServiceService.java
@@ -2,9 +2,13 @@ package ir.maktab.homeserviceprovider.domain.service.service;
 
 import ir.maktab.homeserviceprovider.domain.model.service.ExpertServiceModel;
 import ir.maktab.homeserviceprovider.domain.model.service.ExpertServiceModelId;
+import ir.maktab.homeserviceprovider.domain.model.service.ServiceModel;
+import ir.maktab.homeserviceprovider.domain.model.user.ExpertModel;
 import ir.maktab.homeserviceprovider.domain.service.BaseService;
 import ir.maktab.homeserviceprovider.dto.service.ExpertServiceDto;
 import ir.maktab.homeserviceprovider.repository.service.ExpertServiceRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +19,14 @@ public class ExpertServiceService extends BaseService<ExpertServiceModel, Expert
     public ExpertServiceService(ExpertServiceRepository expertServiceRepository) {
         super(expertServiceRepository);
         this.repository = expertServiceRepository;
+    }
+
+    public Page<ExpertServiceModel> findAllByService(ServiceModel service, Pageable pageable) {
+        return repository.findAllByService(service, pageable);
+    }
+
+    public Page<ExpertServiceModel> findAllByExpert(ExpertModel expert, Pageable pageable) {
+        return repository.findAllByExpert(expert, pageable);
     }
 
     @Override
