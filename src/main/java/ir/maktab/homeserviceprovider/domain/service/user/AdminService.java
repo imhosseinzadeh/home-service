34affa8e7de4872780ada59ed887140e976cd3f7
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-27 18:04:39 +0330

    feat(service): add methods to admin can add Service and subService

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/user/AdminService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/user/AdminService.java
@@ -1,6 +1,10 @@
 package ir.maktab.homeserviceprovider.domain.service.user;
 
 import ir.maktab.homeserviceprovider.domain.model.user.AdminModel;
+import ir.maktab.homeserviceprovider.domain.service.service.ServiceService;
+import ir.maktab.homeserviceprovider.domain.service.service.SubServiceService;
+import ir.maktab.homeserviceprovider.dto.service.ServiceDto;
+import ir.maktab.homeserviceprovider.dto.service.SubServiceDto;
 import ir.maktab.homeserviceprovider.dto.user.AdminDto;
 import ir.maktab.homeserviceprovider.dto.user.param.UserSearchParam;
 import ir.maktab.homeserviceprovider.mapper.user.AdminMapper;
@@ -9,16 +13,23 @@ import ir.maktab.homeserviceprovider.specification.AdminSpecifications;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class AdminService extends UserService<AdminModel, AdminDto> {
 
     private final AdminRepository repository;
+    private final ServiceService serviceService;
+    private final SubServiceService subServiceService;
     private final AdminMapper mapper;
 
-    public AdminService(AdminRepository adminRepository, AdminMapper mapper) {
+    public AdminService(AdminRepository adminRepository, ServiceService serviceService, SubServiceService subServiceService, AdminMapper mapper) {
         super(adminRepository);
         this.repository = adminRepository;
+        this.serviceService = serviceService;
+        this.subServiceService = subServiceService;
         this.mapper = mapper;
     }
 
@@ -35,6 +46,16 @@ public class AdminService extends UserService<AdminModel, AdminDto> {
     @Override
     protected void updateModelByDto(AdminDto dto, AdminModel model) {
 
+    }
+
+    @Transactional
+    public Optional<ServiceDto> addService(ServiceDto serviceDto) {
+        return this.serviceService.save(serviceDto);
+    }
+
+    @Transactional
+    public Optional<SubServiceDto> addSubService(SubServiceDto subServiceDto) {
+        return this.subServiceService.save(subServiceDto);
     }
 
     @Override
