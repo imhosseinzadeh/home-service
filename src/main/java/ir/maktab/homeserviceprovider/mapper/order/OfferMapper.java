8e2ba9d02f038e1e6b1472e10a037a9e84689d07
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-28 11:32:37 +0330

    feat(mapper): use repository to get reference object by id

--- src/main/java/ir/maktab/homeserviceprovider/mapper/order/OfferMapper.java
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/order/OfferMapper.java
@@ -2,13 +2,22 @@ package ir.maktab.homeserviceprovider.mapper.order;
 
 import ir.maktab.homeserviceprovider.domain.model.order.OfferModel;
 import ir.maktab.homeserviceprovider.dto.order.OfferDto;
+import ir.maktab.homeserviceprovider.exception.DataNotExistsException;
+import ir.maktab.homeserviceprovider.repository.order.OrderRepository;
+import ir.maktab.homeserviceprovider.repository.user.ExpertRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
 public abstract class OfferMapper {
+
+    @Autowired
+    protected ExpertRepository expertRepository;
+    @Autowired
+    protected OrderRepository orderRepository;
 
     @Mappings({
             @Mapping(target = "expertId", source = "expert.id"),
@@ -17,10 +26,10 @@ public abstract class OfferMapper {
     public abstract OfferDto mapToDto(OfferModel model);
 
     @Mappings({
-            @Mapping(target = "expert.id", source = "expertId"),
-            @Mapping(target = "order.id", source = "orderId")
+            @Mapping(target = "expert", expression = "java(expertRepository.getById(dto.getExpertId()))"),
+            @Mapping(target = "order", expression = "java(orderRepository.getById(dto.getOrderId()))")
     })
-    public abstract OfferModel mapToModel(OfferDto dto);
+    public abstract OfferModel mapToModel(OfferDto dto) throws DataNotExistsException;
 
     @Mappings({
             @Mapping(target = "expertId", source = "expert.id"),
@@ -29,8 +38,8 @@ public abstract class OfferMapper {
     public abstract void updateDtoByModel(@MappingTarget OfferDto dto, OfferModel model);
 
     @Mappings({
-            @Mapping(target = "expert.id", source = "expertId"),
-            @Mapping(target = "order.id", source = "orderId")
+            @Mapping(target = "expert", expression = "java(expertRepository.getById(dto.getExpertId()))"),
+            @Mapping(target = "order", expression = "java(orderRepository.getById(dto.getOrderId()))")
     })
     public abstract void updateModelByDto(@MappingTarget OfferModel model, OfferDto dto);
 
--- src/main/java/ir/maktab/homeserviceprovider/mapper/service/SubServiceMapper.java
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/service/SubServiceMapper.java
@@ -2,23 +2,28 @@ package ir.maktab.homeserviceprovider.mapper.service;
 
 import ir.maktab.homeserviceprovider.domain.model.service.SubServiceModel;
 import ir.maktab.homeserviceprovider.dto.service.SubServiceDto;
+import ir.maktab.homeserviceprovider.repository.service.ServiceRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
 public abstract class SubServiceMapper {
 
+    @Autowired
+    protected ServiceRepository serviceRepository;
+
     @Mapping(target = "serviceId", source = "service.id")
     public abstract SubServiceDto mapToDto(SubServiceModel model);
 
-    @Mapping(target = "service.id", source = "serviceId")
+    @Mapping(target = "service", expression = "java(serviceRepository.findById(dto.getServiceId()).get())")
     public abstract SubServiceModel mapToModel(SubServiceDto dto);
 
     @Mapping(target = "serviceId", source = "service.id")
     public abstract void updateDtoByModel(@MappingTarget SubServiceDto dto, SubServiceModel model);
 
-    @Mapping(target = "service.id", source = "serviceId")
+    @Mapping(target = "service", expression = "java(serviceRepository.findById(dto.getServiceId()).get())")
     public abstract void updateModelByDto(@MappingTarget SubServiceModel model, SubServiceDto dto);
 
 }
