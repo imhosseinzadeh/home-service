2cbbe2c120ed97e51eb3b0d78ad1907848cc7b00
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-28 15:54:04 +0330

    feat(mapper): use setter injection

--- src/main/java/ir/maktab/homeserviceprovider/mapper/AbstractGenericMapper.java
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/AbstractGenericMapper.java
@@ -1,7 +1,6 @@
 package ir.maktab.homeserviceprovider.mapper;
 
 import ir.maktab.homeserviceprovider.domain.model.BaseModel;
-import ir.maktab.homeserviceprovider.dto.BaseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
@@ -9,19 +8,22 @@ import org.springframework.stereotype.Component;
 import java.io.Serializable;
 
 @Component
-public abstract class AbstractGenericMapper<M extends BaseModel<I>, D extends BaseDto<I>, I extends Serializable>
-        implements GenericMapper<M, D> {
+public abstract class AbstractGenericMapper<M extends BaseModel<I>, I extends Serializable> {
 
-    @Autowired
     private JpaRepository<M, I> jpaRepository;
 
-    protected M map(I id) {
+    public M findModelById(I id) {
         if (id == null) {
             return null;
         }
 
         return this.jpaRepository.findById(id)
                 .orElse(null);
+    }
+
+    @Autowired
+    public void setJpaRepository(JpaRepository<M, I> jpaRepository) {
+        this.jpaRepository = jpaRepository;
     }
 
 }
