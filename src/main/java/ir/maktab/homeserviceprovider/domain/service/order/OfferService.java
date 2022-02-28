991f4eb2a25afbe9a40cc8ae3965173310917a0f
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-28 22:53:40 +0330

    feat(controller): impl save of Offer

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/order/OfferService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/order/OfferService.java
@@ -3,17 +3,43 @@ package ir.maktab.homeserviceprovider.domain.service.order;
 import ir.maktab.homeserviceprovider.domain.model.order.OfferModel;
 import ir.maktab.homeserviceprovider.domain.service.BaseService;
 import ir.maktab.homeserviceprovider.dto.order.OfferDto;
+import ir.maktab.homeserviceprovider.dto.order.OrderDto;
+import ir.maktab.homeserviceprovider.mapper.order.OfferMapper;
 import ir.maktab.homeserviceprovider.repository.order.OfferRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
 public class OfferService extends BaseService<OfferModel, OfferDto, Long> {
 
     private final OfferRepository repository;
+    private final OrderService orderService;
 
-    public OfferService(OfferRepository offerRepository) {
+    private final OfferMapper offerMapper;
+
+    public OfferService(OfferRepository offerRepository, OrderService orderService, OfferMapper offerMapper) {
         super(offerRepository);
         this.repository = offerRepository;
+        this.orderService = orderService;
+        this.offerMapper = offerMapper;
+    }
+
+    @Override
+    public Optional<OfferDto> save(OfferDto dto) {
+        Optional<OrderDto> optOrder = this.orderService.findById(dto.getOrderId());
+        if (optOrder.isPresent()) {
+            BigDecimal orderPrice = optOrder.get().getProposedPrice();
+            BigDecimal offerPrice = dto.getProposalPrice();
+
+            if (offerPrice.compareTo(orderPrice) > 0) {
+                OfferModel saved = this.repository.save(this.offerMapper.mapToModel(dto));
+                return Optional.of(this.offerMapper.mapToDto(saved));
+            }
+            //throw LesserPriceException();
+        }
+        return Optional.empty();
     }
 
     @Override
