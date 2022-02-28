fc3d3a889d84b32cf01b700daadb66ba486c3629
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-28 13:13:32 +0330

    feat(mapper): add base for mapper

---
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/GenericMapper.java
@@ -0,0 +1,15 @@
+package ir.maktab.homeserviceprovider.mapper;
+
+import org.mapstruct.MappingTarget;
+
+public interface GenericMapper<M, D> {
+
+    D mapToDto(M model);
+
+    M mapToModel(D dto);
+
+    void updateDtoByModel(@MappingTarget D dto, M model);
+
+    void updateModelByDto(@MappingTarget M model, D dto);
+
+}
