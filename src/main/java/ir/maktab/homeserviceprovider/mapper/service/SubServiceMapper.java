a5956ef9684f61a7dca9261c121c5a439591a0e3
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-28 13:15:20 +0330

    feat(mapper): impl AbstractGenericMapper

--- src/main/java/ir/maktab/homeserviceprovider/mapper/service/SubServiceMapper.java
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/service/SubServiceMapper.java
@@ -2,28 +2,15 @@ package ir.maktab.homeserviceprovider.mapper.service;
 
 import ir.maktab.homeserviceprovider.domain.model.service.SubServiceModel;
 import ir.maktab.homeserviceprovider.dto.service.SubServiceDto;
-import ir.maktab.homeserviceprovider.repository.service.ServiceRepository;
+import ir.maktab.homeserviceprovider.mapper.AbstractGenericMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
-public abstract class SubServiceMapper {
+@Mapper(componentModel = "spring", uses = {ServiceMapper.class})
+public abstract class SubServiceMapper extends AbstractGenericMapper<SubServiceModel, SubServiceDto, Long> {
 
-    @Autowired
-    protected ServiceRepository serviceRepository;
-
-    @Mapping(target = "serviceId", source = "service.id")
-    public abstract SubServiceDto mapToDto(SubServiceModel model);
-
-    @Mapping(target = "service", expression = "java(serviceRepository.findById(dto.getServiceId()).get())")
+    @Override
+    @Mapping(target = "service", source = "serviceId")
     public abstract SubServiceModel mapToModel(SubServiceDto dto);
-
-    @Mapping(target = "serviceId", source = "service.id")
-    public abstract void updateDtoByModel(@MappingTarget SubServiceDto dto, SubServiceModel model);
-
-    @Mapping(target = "service", expression = "java(serviceRepository.findById(dto.getServiceId()).get())")
-    public abstract void updateModelByDto(@MappingTarget SubServiceModel model, SubServiceDto dto);
 
 }
