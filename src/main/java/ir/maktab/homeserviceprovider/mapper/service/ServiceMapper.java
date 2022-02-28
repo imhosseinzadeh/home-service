4d891ef06443493c01199588197618f1bc3a3755
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-02-28 13:14:43 +0330

    feat(mapper): impl AbstractGenericMapper

--- src/main/java/ir/maktab/homeserviceprovider/mapper/service/ServiceMapper.java
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/service/ServiceMapper.java
@@ -2,18 +2,13 @@ package ir.maktab.homeserviceprovider.mapper.service;
 
 import ir.maktab.homeserviceprovider.domain.model.service.ServiceModel;
 import ir.maktab.homeserviceprovider.dto.service.ServiceDto;
+import ir.maktab.homeserviceprovider.mapper.AbstractGenericMapper;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
-public abstract class ServiceMapper {
-
-    public abstract ServiceDto mapToDto(ServiceModel model);
-
-    public abstract ServiceModel mapToModel(ServiceDto dto);
-
-    public abstract void updateDtoByModel(@MappingTarget ServiceDto dto, ServiceModel model);
-
-    public abstract void updateModelByDto(@MappingTarget ServiceModel model, ServiceDto dto);
-
+public abstract class ServiceMapper extends AbstractGenericMapper<ServiceModel, ServiceDto, Long> {
+    @Override
+    protected ServiceModel map(Long id) {
+        return super.map(id);
+    }
 }
