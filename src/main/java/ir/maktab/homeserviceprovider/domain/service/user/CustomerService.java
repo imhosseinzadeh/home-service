84f322a86a0ba0a0331e7a0aae9140b6262905f6
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-03-01 13:41:00 +0330

    feat(service): user can get his orders history

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/user/CustomerService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/user/CustomerService.java
@@ -1,8 +1,12 @@
 package ir.maktab.homeserviceprovider.domain.service.user;
 
+import ir.maktab.homeserviceprovider.domain.model.order.OrderModel;
 import ir.maktab.homeserviceprovider.domain.model.user.CustomerModel;
+import ir.maktab.homeserviceprovider.dto.order.OrderDto;
 import ir.maktab.homeserviceprovider.dto.user.CustomerDto;
 import ir.maktab.homeserviceprovider.dto.user.param.UserSearchParam;
+import ir.maktab.homeserviceprovider.exception.DataNotExistsException;
+import ir.maktab.homeserviceprovider.mapper.order.OrderMapper;
 import ir.maktab.homeserviceprovider.mapper.user.CustomerMapper;
 import ir.maktab.homeserviceprovider.repository.user.CustomerRepository;
 import ir.maktab.homeserviceprovider.specification.CustomerSpecifications;
@@ -10,16 +14,30 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Set;
+
 @Service
 public class CustomerService extends UserService<CustomerModel, CustomerDto> {
 
     private final CustomerRepository repository;
     private final CustomerMapper customerMapper;
+    private final OrderMapper orderMapper;
 
-    public CustomerService(CustomerRepository repository, CustomerMapper mapper) {
+    public CustomerService(CustomerRepository repository, CustomerMapper mapper, OrderMapper orderMapper) {
         super(repository, mapper);
         this.repository = repository;
         this.customerMapper = mapper;
+        this.orderMapper = orderMapper;
+    }
+
+    public Set<OrderDto> ordersHistory(Long customerId) throws DataNotExistsException {
+        Optional<CustomerModel> optCustomer = this.repository.findById(customerId);
+        if (optCustomer.isPresent()) {
+            Set<OrderModel> customerOrders = optCustomer.get().getOrders();
+            return orderMapper.mapToDto(customerOrders);
+        }
+        throw new DataNotExistsException();
     }
 
     @Override
