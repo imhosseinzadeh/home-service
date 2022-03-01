d3d12dc1f4a2491441ff84d1fe1ad2e04d49b640
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-03-01 11:05:38 +0330

    refactor(model): change values

--- src/main/java/ir/maktab/homeserviceprovider/domain/model/order/OrderModelStatus.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/model/order/OrderModelStatus.java
@@ -1,12 +1,12 @@
 package ir.maktab.homeserviceprovider.domain.model.order;
 
 public enum OrderModelStatus {
-    WAITING_FOR_EXPERTS_SUGGESTIONS(1),
-    WAITING_FOR_CHOICE_EXPERT(2),
-    WAITING_FOR_EXPERT_TO_COME_TO_YOUR_PLACE(3),
-    STARTED(3),
-    DONE(4),
-    PAID(5);
+    WAIT_FOR_EXPERTS_SUGGESTIONS(1),
+    WAIT_FOR_ACCEPT_EXPERT_OFFER(2),
+    WAIT_FOR_EXPERT_TO_COME_TO_YOUR_PLACE(3),
+    STARTED(4),
+    DONE(5),
+    PAID(6);
 
     private final int value;
 
