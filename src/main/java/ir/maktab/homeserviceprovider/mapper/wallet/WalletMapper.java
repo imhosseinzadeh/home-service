035b85e90aff37ccf0cbd0964c7176b9dee1d486
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-03-01 13:01:16 +0330

    feat(dto): define targets and sources

--- src/main/java/ir/maktab/homeserviceprovider/mapper/wallet/WalletMapper.java
+++ src/main/java/ir/maktab/homeserviceprovider/mapper/wallet/WalletMapper.java
@@ -3,9 +3,24 @@ package ir.maktab.homeserviceprovider.mapper.wallet;
 import ir.maktab.homeserviceprovider.domain.model.wallet.WalletModel;
 import ir.maktab.homeserviceprovider.dto.wallet.WalletDto;
 import ir.maktab.homeserviceprovider.mapper.AbstractMapper;
+import ir.maktab.homeserviceprovider.mapper.user.CustomerMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {CustomerMapper.class})
 public abstract class WalletMapper extends AbstractMapper<WalletModel, WalletDto, Long> {
+
+    @Mapping(target = "userId", source = "user.id")
+    public abstract WalletDto mapToDto(WalletModel model);
+
+    @Mapping(target = "user", source = "userId")
+    public abstract WalletModel mapToModel(WalletDto dto);
+
+    @Mapping(target = "userId", source = "user.id")
+    public abstract void updateDtoByModel(@MappingTarget WalletDto dto, WalletModel model);
+
+    @Mapping(target = "user", source = "userId")
+    public abstract void updateModelByDto(@MappingTarget WalletModel model, WalletDto dto);
 
 }
