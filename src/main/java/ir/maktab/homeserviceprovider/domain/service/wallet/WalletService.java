a9d61d786991973bf07e28f02e018a25bd125aab
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-03-01 13:06:21 +0330

    feat(service): user can see wallet balance

--- src/main/java/ir/maktab/homeserviceprovider/domain/service/wallet/WalletService.java
+++ src/main/java/ir/maktab/homeserviceprovider/domain/service/wallet/WalletService.java
@@ -3,9 +3,14 @@ package ir.maktab.homeserviceprovider.domain.service.wallet;
 import ir.maktab.homeserviceprovider.domain.model.wallet.WalletModel;
 import ir.maktab.homeserviceprovider.domain.service.BaseService;
 import ir.maktab.homeserviceprovider.dto.wallet.WalletDto;
+import ir.maktab.homeserviceprovider.exception.DataNotExistsException;
 import ir.maktab.homeserviceprovider.mapper.wallet.WalletMapper;
 import ir.maktab.homeserviceprovider.repository.wallet.WalletRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
 public class WalletService extends BaseService<WalletModel, WalletDto, Long> {
@@ -18,4 +23,14 @@ public class WalletService extends BaseService<WalletModel, WalletDto, Long> {
         this.repository = repository;
         this.walletMapper = walletMapper;
     }
+
+    @Transactional(readOnly = true)
+    public BigDecimal seeBalance(Long walletId) throws DataNotExistsException {
+        Optional<WalletModel> optWallet = repository.findById(walletId);
+        if (optWallet.isPresent()) {
+            return optWallet.get().getBalance();
+        }
+        throw new DataNotExistsException("");
+    }
+
 }
